2c49a7e85063bf94e0dcfb87bb5fefc9b83af85c
Heine Stokholm <hstokholm@gmail.com>
2021-12-13 22:51:11 +0100

    day3 part 1

--- src/day03.zig
+++ src/day03.zig
@@ -1,70 +1,41 @@
 const std = @import("std");
 const print = std.debug.print;
 
-const data = @embedFile("../data/day02.txt");
-
-const Direction = enum {
-    up,
-    forward,
-    down,
-    number,
-};
+const data = @embedFile("../data/day03.txt");
 
 fn day01() void {
     var iterator = std.mem.tokenize(u8, data, "\r\n ");
-    var depth: i32 = 0;
-    var distance: i32 = 0;
-    var command: Direction = Direction.number;
-    while (iterator.next()) |token| {
-        switch (command) {
-            Direction.up => {
-                depth -= std.fmt.parseUnsigned(u16, token, 10) catch @panic("asdf");
-                command = Direction.number;
-            },
-            Direction.down => {
-                depth += std.fmt.parseUnsigned(u16, token, 10) catch @panic("asdf");
-                command = Direction.number;
-            },
-            Direction.forward => {
-                distance += std.fmt.parseUnsigned(u16, token, 10) catch @panic("asdf");
-                command = Direction.number;
-            },
-            Direction.number => {
-                command = std.meta.stringToEnum(Direction, token).?;
-            },
-        }
-    }
-    print("Day 02 - depth : {}, distance : {}, multiplied :{}\n", .{ depth, distance, depth * distance });
-}
 
-fn day02() void {
-    var iterator = std.mem.tokenize(u8, data, "\r\n ");
-    var depth: i32 = 0;
-    var distance: i32 = 0;
-    var aim: i32 = 0;
-    var command: Direction = Direction.number;
+    var counts = [_]i32{0} ** 12;
+
     while (iterator.next()) |token| {
-        switch (command) {
-            Direction.up => {
-                aim -= std.fmt.parseUnsigned(u16, token, 10) catch @panic("asdf");
-                command = Direction.number;
-            },
-            Direction.down => {
-                aim += std.fmt.parseUnsigned(u16, token, 10) catch @panic("asdf");
-                command = Direction.number;
-            },
-            Direction.forward => {
-                const f = std.fmt.parseUnsigned(u16, token, 10) catch @panic("asdf");
-                distance += f;
-                depth += f * aim;
-                command = Direction.number;
-            },
-            Direction.number => {
-                command = std.meta.stringToEnum(Direction, token).?;
-            },
+        if (token.len >= 12) {
+            var idx: usize = 0;
+            while (idx < 12) : (idx += 1) {
+                if (token[idx] == '1') {
+                    counts[idx] += 1;
+                } else {
+                    counts[idx] -= 1;
+                }
+            }
         }
     }
-    print("Day 02 - depth : {}, distance : {}, multiplied :{}\n", .{ depth, distance, depth * distance });
+    var idx: usize = 0;
+    var gamma: i23 = 0;
+    var epsilon : i23 = 0;
+    while (idx < 12) : (idx += 1) {
+        gamma <<= 1;
+        epsilon <<= 1;
+        if(counts[idx] > 0) {
+            gamma |= 1;
+        } else {
+            epsilon |= 1;
+        }
+        print("{d} ", .{counts[idx]});
+    }
+        print("\n{d} ", .{gamma});
+        print("\n{d}\n ", .{epsilon});
+        print("{d}\n", .{gamma*epsilon});
 }
 
 pub fn main() !void {
@@ -72,9 +43,5 @@ pub fn main() !void {
     day01();
     var part01 = timer.lap();
     print("Day 02 - part 01 took {:15}ns\n", .{part01});
-    timer.reset();
-    day02();
-    var part02 = timer.lap();
-    print("Day 02 - part 02 took {:15}ns\n", .{part02});
     timer.reset();
 }
